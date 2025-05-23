dc32021e54fe00791c8cecb9ec8c9a26e9861346
Levi <lstoll@exeter.edu>
2025-05-23 12:08:16 -0400

    circle

--- ball.py
+++ ball.py
@@ -1,24 +1,23 @@
 import pygame
 from random import randint
 BLACK = (0, 0, 0)
- 
+
 class Ball(pygame.sprite.Sprite):
-    
-    
     def __init__(self, color, width, height):
-        
         super().__init__()
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface([width, height], pygame.SRCALPHA)
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        # Draw a circle instead of a rectangle
+        pygame.draw.circle(self.image, color, (width // 2, height // 2), width // 2)
         self.velocity = [randint(4,8),randint(-8,8)]
         self.rect = self.image.get_rect()
-        
+        self.mask = pygame.mask.from_surface(self.image)  # For accurate collision
+
     def update(self):
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
-          
+
     def bounce(self):
         self.velocity[1] = -self.velocity[1]
         if self.velocity[0] == 0:
